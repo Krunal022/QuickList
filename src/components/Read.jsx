ce9f79a1f9a604bbbb1aa106584167cfc07d6b84
Krunal022 <krunalvaghela723@gmail.com>
2025-06-03 15:04:06 +0530

    improved layout with Tailwind CSS

--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-
 const Read = (props) => {
   const todos = props.todos;
   const settodos = props.settodos;
+  
+  const DeleteHandler = (id) => {
+    const filtertodo = todos.filter((todo) => todo.id != id);
+    settodos(filtertodo);
+  };
+
   const render = todos.map((item) => {
-    const DeleteHandler = (id) => {
-      const filtertodo = todos.filter((todo) => todo.id != id);
-      settodos(filtertodo);
-    };
     return (
       <li
-        className="bg-slate-300 list-decimal text-black rounded-xl py-2 px-5 mb-2 flex justify-between items-center"
+        className="bg-slate-300 text-black rounded-xl py-2 px-5 mb-2 flex justify-between items-center"
         key={item.id}
       >
         {item.title}
@@ -23,14 +23,16 @@ const Read = (props) => {
       </li>
     );
   });
-  // console.log(render);
+
   return (
     <div className="w-[50%] p-10">
       <h1 className="text-5xl font-thin mb-10">
         <span className="text-gray-600">You’ve</span> Added{" "}
         <span className="text-indigo-600">These</span>!
       </h1>
-      <ol className="text-xl list-disc pl-6 font-thin text-[2rem]">{render}</ol>
+      <ol className="text-2xl pl-6 font-thin list-decimal">
+        {render}
+      </ol>
     </div>
   );
 };
